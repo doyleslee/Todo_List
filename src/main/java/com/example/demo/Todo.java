410df0aed5e3a448cd0cf0ecf6c8a68c0dba2420
GBTC408011UR <GBTC408011UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-14 16:54:51 -0400

    first commit, no styling yet

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1)
+    private String task;
+
+    @NotNull
+    @Size(min=1)
+    private String priority;
+
+    @Size(max=150)
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
